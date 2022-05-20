08290908b5ef26c068f97b14b74e72fdd60f1ec7
Kiara <kiarabroekhuizen1@hotmail.com>
2022-05-20 19:51:19 +0200

    form works

--- index.js
+++ index.js
@@ -15,11 +15,27 @@ app.use(express.urlencoded({ extended: true }));
 
 //routing
 app.get('/', index)
+app.get('/complete', renderCompletePage)
+app.post('/complete', postCompletePage)
+app.get('/end', renderEndPage)
+app.post('/end', postEndPage)
 
 function index(req, res) {
     res.render('index')
 }
+function renderCompletePage(req, res) {
+    res.render('complete')
+}
+function postCompletePage(req, res) {
+    res.render('complete')
+}
 
+function renderEndPage(req, res) {
+    res.render('end')
+}
+function postEndPage(req, res) {
+    res.render('end')
+}
 function server() {
     console.log('The server is running succesfully! 🎉');
 }
