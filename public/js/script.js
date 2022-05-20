f4474f4f2bbac0631d745a71ba434d6ee7c982a5
Kiara <kiarabroekhuizen1@hotmail.com>
2022-05-20 23:33:56 +0200

    PD works

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,59 @@
 console.log('are we connected?')
 
 var fieldset;
-var studentIdField = document.getElementById('studentId')
+var element = document.getElementById('studentId')
 var isValid;
-var submitBtn = document.querySelector('.submit')
-var textArea;
 
 
-studentIdField.addEventListener('keyup', function (e) {
-    e.preventDefault()
-    isValid = studentIdField.checkValidity()
-    //  console.log(isValid)
-    if (isValid == true) {
-        fieldset = document.querySelector('fieldset:nth-child(2)');
-        fieldset.classList.add('show')
-    }
-});
 
-textArea = document.querySelector('textarea:last-of-type');
+// element.addEventListener('keyup', function (e) {
+//     e.preventDefault()
+//     isValid = element.checkValidity()
+//     //  console.log(isValid)
+//     if (isValid == true) {
+//         fieldset = document.querySelector('fieldset:nth-child(2)');
+//         fieldset.classList.add('show')
+//     }
+// });
 
-textArea.addEventListener('keyup', function (e) {
-    e.preventDefault()
-    isValid = textArea.checkValidity()
-    if (isValid == true) {
-        fieldset = document.querySelector('fieldset:nth-child(3)');
-        fieldset.classList.add('show')
-    }
 
-})
+function showEl(element, fieldset) {
+    element.addEventListener('keyup', function (e) {
+        e.preventDefault()
+        isValid = element.checkValidity()
+        //  console.log(isValid)
+        if (isValid == true) {
+            fieldset.classList.add('show')
+        }
+    });
+}
+
+element = document.querySelector('#studentId');
+fieldset = document.querySelector('#wafs');
+showEl(element, fieldset)
+
+element = document.querySelector('#inzicht-wafs');
+fieldset = document.querySelector('#css');
+showEl(element, fieldset)
+
+element = document.querySelector('#inzicht-css');
+fieldset = document.querySelector('#pwa');
+showEl(element, fieldset)
+
+element = document.querySelector('#inzicht-pwa');
+fieldset = document.querySelector('#bt');
+showEl(element, fieldset)
+
+element = document.querySelector('#inzicht-bt');
+fieldset = document.querySelector('#rtw');
+showEl(element, fieldset)
+
+element = document.querySelector('#inzicht-rtw');
+fieldset = document.querySelector('#hcd');
+showEl(element, fieldset)
+
+element = document.querySelector('#inzicht-hcd');
+fieldset = document.querySelector('#submit');
+showEl(element, fieldset)
 
 
