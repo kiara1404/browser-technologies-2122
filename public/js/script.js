577da23e49d6e6b821dcede684a346f3c37c5865
Kiara <kiarabroekhuizen1@hotmail.com>
2022-05-21 22:26:33 +0200

    added validation

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,22 @@ console.log('are we connected?')
 
 
 const form = document.querySelector('form');
-const errorMsg = document.querySelector('.error')
-const submit = document.querySelector('button[type=submit]')
+const inputs = document.getElementsByTagName('input');
+const textareas = document.getElementsByTagName('textarea');
+const errorMsg = document.querySelector('.error');
+const submit = document.querySelector('button[type=submit]');
+
+
+function addInvalidBorder() {
+    if (form.checkValidity() === false) {
+        for (let i = 0; i < inputs.length; i += 1) {
+            inputs[i].classList.add('invalid')
+        }
+        for (let i = 0; i < textareas.length; i += 1) {
+            textareas[i].classList.add('invalid')
+        }
+    }
+}
 
 function isValid() {
     if (form.checkValidity() === true) {
@@ -16,13 +30,23 @@ function isValid() {
 function removeErrorMsg() {
     errorMsg.classList.remove('show')
 }
+function removeInvalidBorder() {
+    for (let i = 0; i < inputs.length; i += 1) {
+        inputs[i].classList.remove('invalid')
+    }
+    for (let i = 0; i < textareas.length; i += 1) {
+        textareas[i].classList.remove('invalid')
+    }
 
+}
 
 submit.addEventListener('click', () => {
     isValid()
+    addInvalidBorder()
 })
 
 form.addEventListener('keyup', () => {
     removeErrorMsg()
+    removeInvalidBorder()
 })
 
