3cdfa47b8965720f1e09202e982aacc73da51b7f
Kiara <kiarabroekhuizen1@hotmail.com>
2022-05-21 23:37:29 +0200

    started with saving data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs')
 require('dotenv').config();
 const port = process.env.PORT || 1000;
 
@@ -11,6 +12,7 @@ app.set('views', './views');
 
 // public folder location
 app.use(express.static('public'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //routing
@@ -21,10 +23,10 @@ app.get('/pwa', renderPWA)
 app.get('/bt', renderBT)
 app.get('/rtw', renderRTW)
 app.get('/hcd', renderHCD)
-app.get('/complete', renderCompletePage)
 app.get('/end', renderEndPage)
 
-app.post('/complete', postCompletePage)
+
+// POST
 app.post('/end', postEndPage)
 app.post('/wafs', postWafs)
 app.post('/css', postCSS)
@@ -33,9 +35,38 @@ app.post('/bt', postBT)
 app.post('/rtw', postRTW)
 app.post('/hcd', postHCD)
 
-function index(req, res) {
-    res.render('pages/index')
+
+
+const dataFromEnqete = (req) => {
+    return {
+        "docent1": req.body.docent1,
+        "docent2": req.body.docent2,
+        "startPeriode": req.body['start-periode'],
+        "eindPeriode": req.body['eind-periode'],
+        "beoordeling": req.body.beoordeling,
+        "moeilijkheid": req.body.moeilijkheid,
+        "moeilijkheidToelichting": req.body['moeilijkheid-toelichting'],
+        "uitleg": req.body.uitleg,
+        "uitlegToelichting": req.body['uitleg-toelichting'],
+        "inzicht": req.body.inzicht,
+        "inzichtToelichting": req.body['inzicht-toelichting']
+    }
 }
+
+let stringData;
+
+
+function index(req, res) {
+    fs.readFile('public/json/student.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        let surveyData;
+        if (data) {
+            surveyData = JSON.parse(data)
+        }
+        res.render('pages/index', { student: surveyData })
+    })
+}
+
 function renderWafs(req, res) {
     res.render('pages/wafs')
 }
@@ -55,37 +86,104 @@ function renderHCD(req, res) {
     res.render('pages/hcd')
 }
 
+
 function postWafs(req, res) {
+
+    const student = {
+        "name": req.body.name,
+        "studentId": req.body.studentId
+    }
+    stringData = JSON.stringify(student);
+    console.log(stringData)
+
+    fs.writeFile('public/json/student.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('pages/wafs')
-}
+};
+
 function postCSS(req, res) {
+    const wafs = dataFromEnqete(req);
+    stringData = JSON.stringify(wafs)
+
+    fs.writeFile('public/json/wafs.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('pages/css')
 }
+
+
 function postPWA(req, res) {
+    const css = dataFromEnqete(req);
+    stringData = JSON.stringify(css)
+
+    fs.writeFile('public/json/css.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('pages/pwa')
 }
+
+
 function postBT(req, res) {
+    const pwa = dataFromEnqete(req);
+    stringData = JSON.stringify(pwa)
+
+    fs.writeFile('public/json/pwa.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('pages/bt')
 }
+
+
 function postRTW(req, res) {
+    const bt = dataFromEnqete(req);
+    stringData = JSON.stringify(bt)
+
+    fs.writeFile('public/json/bt.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('pages/rtw')
 }
+
 function postHCD(req, res) {
+    const rtw = dataFromEnqete(req);
+    stringData = JSON.stringify(rtw)
+
+    fs.writeFile('public/json/rtw.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('pages/hcd')
 }
-function renderCompletePage(req, res) {
-    res.render('complete')
-}
-function postCompletePage(req, res) {
-    res.render('complete')
-}
+
 
 function renderEndPage(req, res) {
     res.render('end')
 }
 function postEndPage(req, res) {
+    const hcd = dataFromEnqete(req);
+    stringData = JSON.stringify(hcd)
+
+    fs.writeFile('public/json/hcd.json', stringData, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
     res.render('end')
 }
+
+
 function server() {
     console.log('The server is running succesfully! 🎉');
 }
